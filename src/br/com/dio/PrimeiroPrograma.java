1121c920645ff6270e7d8f0ccdee83ac2c180981
veniciuspc <veniciuspc@gmail.com>
2023-04-28 11:26:05 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		System.out.println("Hello World!");
+
+	}
+
+}
+class Livro{
+	public String livro;
+	public Integer numPaginas;
+}
